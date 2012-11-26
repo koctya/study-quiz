e0658913eba6577dc7742709a52f0611e291fdcc
Kurt Landrus <klandrus@gmail.com>
2012-11-25 20:51:53 -0500

    moved sass config to development env

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,6 @@ module StudyQuiz
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    config.sass.preferred_syntax = :sass
     
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
